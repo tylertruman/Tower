eb49e268ebef57827e90af0dc5424e8ede769af8
Tyler Truman <trucotyler@gmail.com>
2022-08-23 18:34:28 -0600

    yo

--- Tower.client/src/env.js
+++ Tower.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'tylertruman.us.auth0.com'
+export const clientId = 'https://myapi.com'
+export const audience = 'aYponvB5txjq1f3GfcMZUCkAzGnHNdDP'
